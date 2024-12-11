9d79f1d24fa29897dd083eca6acd02b26abfc3de
EsraaAlaaEl_dien <israaaladin012@gmail.com>
2024-12-11 23:18:42 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+let img = document.querySelector(".img")
+let container = document.querySelector(".container")
+function watches( watch){
+    img.src=watch;
+}
+function colors(color){
+    container.style.background = color;
+}
